2c28d6e8e078ac56ebfc30ce2b2c6d2c30520fd8
malikkamchibekov <elemental-kg@mail.ru>
2022-06-22 14:22:11 +0600

    fifth commit

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Hospital)
+admin.site.register(HeadDoctor)
+admin.site.register(Doctor)
+admin.site.register(Nurse)
+admin.site.register(Patient)
+
+
+
+
